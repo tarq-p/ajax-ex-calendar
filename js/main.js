ba420bb011ed6a7038351ebe37b6c07eea6b7d61
tarq-p <pierluigi.tarquinio90@gmail.com>
2020-04-05 12:12:49 +0200

    Primi 3 punti Ok

---
+++ js/main.js
@@ -0,0 +1,56 @@
+$(document).ready(function () {
+
+    var htmlGiorno = $('#calendar-template').html();
+    var templateGiorno = Handlebars.compile(htmlGiorno);
+
+    // Stampare il mese di Gennaio 2018
+    // Tramite click stampare il mese successivo
+
+    var dataIniziale = moment('2018-01-01');
+    stampaGiorniMese(dataIniziale); // Inizializzazione Calendario
+    stampaFestivi();
+
+    $('.mese-succ').click(function () {
+        dataIniziale.add(1, 'month');
+        stampaGiorniMese(dataIniziale);
+    });
+
+    function stampaFestivi() {
+        $.ajax({
+            url: 'https://flynn.boolean.careers/exercises/api/holidays',
+            method: 'GET',
+            data: {
+                year: 2018,
+                month: 0
+            },
+            success: function (data) {
+                var giorniFestivi = data.response;
+                for (var i = 0; i < giorniFestivi.length; i++) {
+                    var giornoFestivo = giorniFestivi[i];
+                    var nomeFestivo = giornoFestivo.name;
+                    var dataFestivo = giornoFestivo.date;
+                    $('#calendar li[data-day="' + dataFestivo + '"]').addClass('festivo').append(' - ' + nomeFestivo);
+                }
+            }
+        });
+    }
+
+    function stampaGiorniMese(meseDaStampare) {
+        $('#calendar').empty();
+        var standardDay = meseDaStampare.clone();
+        var giorniMese = meseDaStampare.daysInMonth();
+        var nomeMese = meseDaStampare.format('MMMM');
+        $('#nome-mese').text(nomeMese); // Aggiorniamo il nome del mese in top calendar
+        for (var i = 1; i <= giorniMese; i++) {
+            // $('#calendar').append('<li>' + i + ' ' + nomeMese + '</li>');
+            var giornoDaInserire = {
+                day: i + ' ' + nomeMese,
+                dataDay: standardDay.format('YYYY-MM-DD')
+            }
+            var templateFinale = templateGiorno(giornoDaInserire); // Stiamo popolando il template con i dati dell'oggetto
+            $('#calendar').append(templateFinale);
+            standardDay.add(1, 'day');
+        }
+    }
+
+});
