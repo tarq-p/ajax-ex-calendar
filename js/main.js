82662edfa02562f2c5d2d1457ca37e80d064ab37
tarq-p <pierluigi.tarquinio90@gmail.com>
2020-04-06 16:38:51 +0200

    piccole stilizzazioni

--- js/main.js
+++ js/main.js
@@ -1,62 +1,69 @@
-$(document).ready(function () {
+$('document').ready(function(){
 
     var htmlGiorno = $('#calendar-template').html();
     var templateGiorno = Handlebars.compile(htmlGiorno);
 
 
     var dataIniziale = moment('2018-01-01');
-
+    var meseIniziale = dataIniziale.month();
     var limiteIniziale = moment('2018-01-01');
-    var limiteFinale = moment('2018-12-31');
-
+    var limiteFinale = moment('2018-12-01');
     stampaGiorniMese(dataIniziale);
-    stampaFestivi(dataIniziale);
+    stampaFestivi(meseIniziale);
+    $('.mese-prec').prop('disabled', true);
 
-    $('.mese-succ').click(function () {
-        $('.mese-prec').prop('disabled', false);
-        dataIniziale.add(1, 'month');
-        stampaGiorniMese(dataIniziale);
-        stampaFestivi(dataIniziale);
+    $('.mese-succ').click(function(){
+             if(dataIniziale.isSameOrAfter(limiteFinale)){
+                   alert('No way man! ');
+              } else {
+                   $('.mese-prec').prop('disabled', false);
+                  dataIniziale.add(1, 'months');
+                  var mese = dataIniziale.month();
+                  stampaGiorniMese(dataIniziale);
+                  stampaFestivi(mese);
+                  if(dataIniziale.isSameOrAfter(limiteFinale)) {
+                     $('.mese-succ').prop('disabled', true);
+                }
+              }
     });
 
-    $('.mese-prec').click(function () {
-         if(dataIniziale.isSameOrBefore(limiteIniziale)){
-              alert('Hai provato ad hackerarmi! :( ');
+    $('.mese-prec').click(function(){
+        if(dataIniziale.isSameOrBefore(limiteIniziale)){
+              alert('No!');
          } else {
-             dataIniziale.subtract(1, 'month');
+             $('.mese-succ').prop('disabled', false);
+             dataIniziale.subtract(1, 'months');
+             var mese = dataIniziale.month();
              stampaGiorniMese(dataIniziale);
-             stampaFestivi(dataIniziale);
+             stampaFestivi(mese);
              if(dataIniziale.isSameOrBefore(limiteIniziale)) {
                 $('.mese-prec').prop('disabled', true);
            }
          }
-
-
-
-
     });
 
-    function stampaFestivi(variabileMeseCorrente) {
+    function stampaFestivi(mese) {
         $.ajax({
-            url: 'https://flynn.boolean.careers/exercises/api/holidays',
+            url: 'https://flynn.boolean.careers/exercises/api/holidays?',
             method: 'GET',
             data: {
-                year: variabileMeseCorrente.year(),
-                month: variabileMeseCorrente.month()
+                year: 2018,
+                month: mese
             },
-            success: function (data) {
+            success: function (data){
                 var giorniFestivi = data.response;
                 for (var i = 0; i < giorniFestivi.length; i++) {
-                    var giornoFestivo = giorniFestivi[i];
+                    var giornoFestivo = giorniFestivi[i]
                     var nomeFestivo = giornoFestivo.name;
                     var dataFestivo = giornoFestivo.date;
-                    $('#calendar li[data-day="' + dataFestivo + '"]').addClass('festivo').append(' - ' + nomeFestivo);
+                    $('#calendar li[data-day="' + dataFestivo + '"]').addClass('festivo').append('*'  + nomeFestivo);
                 }
             }
         });
-    }
+    };
 
-    function stampaGiorniMese(meseDaStampare) {
+
+    function stampaGiorniMese (meseDaStampare){
         $('#calendar').empty();
         var standardDay = meseDaStampare.clone();
         var giorniMese = meseDaStampare.daysInMonth();
@@ -69,8 +76,8 @@ $(document).ready(function () {
             }
             var templateFinale = templateGiorno(giornoDaInserire);
             $('#calendar').append(templateFinale);
-            standardDay.add(1, 'day');
-        }
-    }
+            standardDay.add(1,'day');
+        };
+    };
 
 });
