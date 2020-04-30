d2fcc783b180365cf13a7230e8d9e116b3d8f9ae
tarq-p <pierluigi.tarquinio90@gmail.com>
2020-04-30 16:47:28 +0200

    final

--- js/main.js
+++ js/main.js
@@ -1,85 +1,108 @@
-$('document').ready(function(){
-
-    var htmlGiorno = $('#calendar-template').html();
-    var templateGiorno = Handlebars.compile(htmlGiorno);
-
-
-    var dataIniziale = moment('2018-01-01');
-    var meseIniziale = dataIniziale.month();
-    var limiteIniziale = moment('2018-01-01');
-    var limiteFinale = moment('2018-12-01');
-    stampaGiorniMese(dataIniziale);
-    stampaFestivi(meseIniziale);
-    $('.mese-prec').prop('disabled', true);
-
-    $('.mese-succ').click(function(){
-             if(dataIniziale.isSameOrAfter(limiteFinale)){
-                   alert('No!');
-              } else {
-                   $('.mese-prec').prop('disabled', false);
-                  dataIniziale.add(1, 'months');
-                  var mese = dataIniziale.month();
-                  stampaGiorniMese(dataIniziale);
-                  stampaFestivi(mese);
-                  if(dataIniziale.isSameOrAfter(limiteFinale)) {
-                     $('.mese-succ').prop('disabled', true);
-                }
-              }
+$(document).ready(function() {
+    // ***************************
+    moment.locale('it');
+    // ***************************
+    var year_ = 2018;
+    var month_ = 0;
+    // ***************************
+    var base = moment({year: year_, month: month_});
+    // ***************************
+    request(base);
+    // ***************************
+    print(base);
+    // ***************************
+    $(document).on('click', '.prev, .next', function() {
+        if ($(this).hasClass('prev')) {
+            prevM(base);
+        } else {
+            nextM(base);
+        }
     });
-
-    $('.mese-prec').click(function(){
-        if(dataIniziale.isSameOrBefore(limiteIniziale)){
-              alert('No!');
-         } else {
-             $('.mese-succ').prop('disabled', false);
-             dataIniziale.subtract(1, 'months');
-             var mese = dataIniziale.month();
-             stampaGiorniMese(dataIniziale);
-             stampaFestivi(mese);
-             if(dataIniziale.isSameOrBefore(limiteIniziale)) {
-                $('.mese-prec').prop('disabled', true);
-           }
-         }
-    });
-
-    function stampaFestivi(mese) {
-        $.ajax({
-            url: 'https://flynn.boolean.careers/exercises/api/holidays?',
-            method: 'GET',
-            data: {
-                year: 2018,
-                month: mese
-            },
-            success: function (data){
-                var giorniFestivi = data.response;
-                for (var i = 0; i < giorniFestivi.length; i++) {
-                    var giornoFestivo = giorniFestivi[i]
-                    var nomeFestivo = giornoFestivo.name;
-                    var dataFestivo = giornoFestivo.date;
-                    $('#calendar li[data-day="' + dataFestivo + '"]').addClass('festivo').append('*'  + nomeFestivo);
-                }
-            }
-        });
-    };
-
-
-    function stampaGiorniMese (meseDaStampare){
-        $('#calendar').empty();
-        var standardDay = meseDaStampare.clone();
-        var giorniMese = meseDaStampare.daysInMonth();
-        var nomeMese = meseDaStampare.format('MMMM');
-        $('#nome-mese').text(nomeMese);
-        for (var i = 1; i <= giorniMese; i++) {
-            var giornoDaInserire = {
-                day: i + ' ' + nomeMese,
-                dataDay: standardDay.format('YYYY-MM-DD')
-            }
-            var templateFinale = templateGiorno(giornoDaInserire);
-            $('#calendar').append(templateFinale);
-            standardDay.add(1,'day');
-        };
-    };
-
-    
-
 });
+// ***************************
+// *-------*function*--------*
+// ***************************
+function request(base) {
+    $.ajax(
+        {
+            url: "https://flynn.boolean.careers/exercises/api/holidays",
+            method: "GET",
+            data: {
+                year : base.year(),
+                month : base.month()
+            },
+            success: function (r) {
+                if (r.success) {
+                    console.log(r.response);
+                    holidays(r.response);
+                } else {
+                    error();
+                }
+            },
+            error: function () {
+                error();
+            }
+        }
+    );
+}
+// ***************************
+function print(base) {
+var builderDay = Handlebars.compile($('#day').html());
+var builderMonth = Handlebars.compile($('#month').html());
+var config_month = {
+    'month' : base.format('MMMM')
+    }
+$('.month').append(builderMonth(config_month));
+for (var i = 1; i <= base.daysInMonth(); i++) {
+    var config_days = {
+        'data': i,
+        'day' : base.date(i).format('dddd D YYYY')
+        }
+    $('.days').append(builderDay(config_days));
+    }
+}
+// ***************************
+function holidays(resp) {
+var builderHol = Handlebars.compile($('#holiday').html());
+for (var i = 0; i < resp.length; i++) {
+    if (moment(resp[i].date).isValid()) {
+        for (var x = 0; x < $('.days li').length; x++) {
+            if (moment(resp[i].date).date() == $('.days li').eq(x).attr('data')) {
+                $('.days li').eq(x).addClass('holiday');
+                $('.days li').eq(x).append(builderHol(resp[i]));
+                }
+            }
+        }
+    }
+}
+// ***************************
+function prevM(base) {
+    base = base.subtract(1, 'months');
+    request(base);
+    $('.month h1').remove();
+    $('.days li').remove();
+    print(base);
+    if (base.month() == 0) {
+        $('.prev').css({'display' : 'none'});
+    } else {
+        $('.next').css({'display' : 'block'});
+    }
+}
+// ***************************
+function nextM(base) {
+    base = base.add(1, 'months');
+    request(base);
+    $('.month h1').remove();
+    $('.days li').remove();
+    print(base);
+    if (base.month() == 11) {
+        $('.next').css({'display' : 'none'});
+    } else {
+        $('.prev').css({'display' : 'block'});
+    }
+}
+// ***************************
+function error() {
+    console.log('ERROR');
+}
+// ***************************
